034f7777be5db2b7e8dc91cf38b731afeaad6f9d
sabamillion <sabamillion@gmail.com>
2024-07-01 21:05:19 -0400

    frontend update

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://evangadiforum-backend-cthg.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
